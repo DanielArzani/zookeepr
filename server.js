4f4cd7d486497e952ba1ed495c997373c7b38bb5
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2021-12-30 10:32:56 -0500

    add POST route and validation

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //* Imports
-const { json } = require("express");
 const express = require("express");
+const fs = require('fs');
+const path = require('path');
 const {animals} = require('./data/animals.json');
 
 
@@ -9,6 +10,12 @@ const {animals} = require('./data/animals.json');
 const app = express();
 // Assign port number
 const PORT = process.env.PORT || 3001;
+
+//* Methods
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 
 
@@ -52,9 +59,33 @@ function findById(id, animalsArray){
     return result;
 } 
 
-//* Routes
+//^ Posts new animal data
+function createNewAnimal(body, animalsArray){
+    const animal = body;
+    animalsArray.push(animal);
 
-//^ All animals route
+    // The null argument means we don't want to edit any of our existing data; if we did, we could pass something in there. The 2 indicates we want to create white space between our values to make it more readable.
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'), 
+        JSON.stringify({animals: animalsArray}, null, 2)
+        );
+
+    return body;
+}
+
+//^ Validate animal data
+function validateAnimal(animal){
+    if(!animal.name || typeof animal.name !== 'string') return false;
+    if(!animal.species || typeof animal.species !== 'string') return false;
+    if(!animal.diet || typeof animal.diet !== 'string') return false;
+    if(!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) return false;
+
+    return true;
+}
+
+//* GET Routes
+
+//^ GET all animals
 app.get("/api/animals", function(req,res){
     let results = animals;
     if(req.query){
@@ -63,12 +94,33 @@ app.get("/api/animals", function(req,res){
     res.json(results);
 });
 
-//^ Specific params route
+//^ GET specific params
 app.get("/api/animals/:id", function(req,res){
     const result = findById(req.params.id, animals);
     if(result) return res.json(result);
     else return res.send(404 + " Sorry, we can't seem to find what you're searching for ");
 });
+
+
+//* POST Routes
+
+//& POST client data 
+app.post("/api/animals", function(req,res){
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if(!validateAnimal(req.body)){
+        res.status(400).send("The animal is not properly formatted");
+    }else{
+        // set id based on what the next index of the array will be
+        const animal = createNewAnimal(req.body, animals);
+    
+        res.json(animal);
+    }
+});
+
+
 
 // Tell server to listen for requests
 app.listen(PORT, function(){
