6dfedc32a53286736bd45068e59a6766d344e00a
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2021-12-29 22:37:10 -0500

    Add Heroku

---
+++ server.js
@@ -0,0 +1,64 @@
+//* Imports
+const { json } = require("express");
+const express = require("express");
+const {animals} = require('./data/animals.json');
+
+
+//* Variables
+// instantiate the server
+const app = express();
+// Assign port number
+const port = 3000;
+
+
+
+//* Functions
+
+//^ Gets query params
+function filterByQuery(query, animalsArray){
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+
+    // Checks to see if personalityTraits is a single trait or an array
+    if(query.personalityTraits){
+        if(typeof query.personalityTraits === "string"){
+            personalityTraitsArray = [query.personalityTraits];
+        }else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+    }
+
+    // Loop through personalityTraitsArray and returns the filtered result that has each trait
+    personalityTraitsArray.forEach((trait)=>{
+        filteredResults = filteredResults.filter(animal => animal.personalityTraits.indexOf(trait) !== -1);
+    })
+
+    // Checks to see if the param entered is the same as the object property then returns a new array with only those values
+    if(query.diet){
+        filteredResults = filteredResults.filter((animal)=>animal.diet === query.diet);
+    }
+    if(query.species){
+        filteredResults = filteredResults.filter((animal)=>animal.species === query.species);
+    }
+    if(query.name){
+        filteredResults = filteredResults.filter((animal)=>animal.name === query.name);
+    }
+    return filteredResults;
+}
+
+//* Routes
+
+//^ All animals route
+app.get("/api/animals", function(req,res){
+    let results = animals;
+    if(req.query){
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+
+// Tell server to listen for requests
+app.listen(process.env.PORT || port, function(){
+    console.log(`API server now on port 3001!`);
+});
